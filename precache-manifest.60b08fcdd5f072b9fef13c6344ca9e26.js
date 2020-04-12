6bd44c4117276656286cb533e144f2aa67ce6e3b
ellord <apps@ellord.net>
2020-04-12 21:13:03 +0900

    Updates

---
+++ precache-manifest.60b08fcdd5f072b9fef13c6344ca9e26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5250a04b5c7d863038ac582967345ade",
+    "url": "/hello-react/index.html"
+  },
+  {
+    "revision": "864560b7290f75e7205d",
+    "url": "/hello-react/static/css/main.66ae833f.chunk.css"
+  },
+  {
+    "revision": "4f5dbbef950693fa8d25",
+    "url": "/hello-react/static/js/2.5eea9fa4.chunk.js"
+  },
+  {
+    "revision": "864560b7290f75e7205d",
+    "url": "/hello-react/static/js/main.a8e63cb1.chunk.js"
+  },
+  {
+    "revision": "d55c10f431b228700b17",
+    "url": "/hello-react/static/js/runtime-main.cf80ab3a.js"
+  }
+]);
